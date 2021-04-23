190c583b596b4af6dc29f6b82c284b9a9491f063
koshea9 <keegan.oshea9@gmail.com>
2021-04-23 09:16:15 -0600

    Fix tests, add controller and view for US 1

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -18,10 +18,10 @@ RSpec.describe "flights index page", type: :feature do
     @passenger_4 = Passenger.create!(name: 'sam', age: 30)
 
     #manifest(join table)
-    @manifest_1 = Manifest.create!(passenger: @passenger_1, flight: @flight_1)
+    @manifest_1 = Manifest.create!(passenger: @passenger_4, flight: @flight_1)
     @manifest_2 = Manifest.create!(passenger: @passenger_2, flight: @flight_1)
     @manifest_3 = Manifest.create!(passenger: @passenger_3, flight: @flight_1)
-    @manifest_4 = Manifest.create!(passenger: @passenger_4, flight: @flight_3)
+    @manifest_4 = Manifest.create!(passenger: @passenger_1, flight: @flight_3)
   end
 
   # I see a list of all flight numbers
@@ -39,5 +39,15 @@ RSpec.describe "flights index page", type: :feature do
   end
 
   it 'lists the names of all passengers under each flight' do
+    visit flights_path
+
+    within "#flight-#{@flight_1.id}" do
+      expect(page).to have_content(@passenger_4.name)
+      expect(page).to have_content(@passenger_2.name)
+      expect(page).to have_content(@passenger_3.name)
+    end
+    within "#flight-#{@flight_3.id}" do
+      expect(page).to have_content(@passenger_1.name)
+    end
   end
 end
