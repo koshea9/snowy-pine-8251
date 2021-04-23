1b13b6ffa558d4759c7cd7b0202e8123e84dd388
koshea9 <keegan.oshea9@gmail.com>
2021-04-23 08:24:20 -0600

    Add airline association

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Airline, type: :model do
   it {should have_many :flights}
+  it {should have_many(:manifests).through(:flights)}
+  it {should have_many(:passengers).through(:manifests)}
 end
